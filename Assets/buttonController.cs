83b6cccb743cc4afa475cfb1181c15bda69d6847
alexrothberg <36311902+alexrothberg@users.noreply.github.com>
2018-09-14 20:23:48 -0400

    Doesnt work

--- Assets/buttonController.cs
+++ Assets/buttonController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class buttonController : MonoBehaviour,IPointerDownHandler {
+public class buttonController : MonoBehaviour, IPointerDownHandler{
 
     //joystick will refer to the combination of light blue bg halo and options text
     //button will refer to the white button with shadow that is always visible
@@ -15,14 +15,14 @@ public class buttonController : MonoBehaviour,IPointerDownHandler {
     bool B_Joystick_Visible = false;
 
     public GameObject Nub;
-   
+
     public GameObject Joystick;
     public Touch touch;
     Vector3 Shrinker = new Vector3(.01f, .01f, .01f);
 
     public void OnPointerDown(PointerEventData eventData)
     {
-       
+
         Nub.SetActive(true);
         Joystick.SetActive(true);
 
@@ -31,76 +31,36 @@ public class buttonController : MonoBehaviour,IPointerDownHandler {
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         if (B_Joystick_Visible) //this checks to see whether the joystick is visible and behaves accordingly
         {
             touch = Input.GetTouch(0);
-
             Nub.transform.position = touch.position;
-            //Debug.Log(touch.position);
-            //Debug.Log(Nub.transform.position);
 
-            //if (touch.phase == TouchPhase.Ended)
-            //{
-            //    Nub.transform.localScale = Vector3.one;
+            ButtonSelected(60);
 
-            //    B_Joystick_Visible = false;
-            //}
-            if (Nub.transform.localPosition.magnitude > 60)
-            {
-                if (Nub.transform.localPosition.x > 0)
-                {
-                    if (Nub.transform.localPosition.y > 0) { Debug.Log("topRight"); }
-                    else { Debug.Log("bottomRight"); }
-                }
-                else
-                {
-                    if (Nub.transform.localPosition.y > 0) { Debug.Log("topLeft"); }
-                    else { Debug.Log("bottomLeft"); }
-                }
-                Nub.transform.localScale = Vector3.one;
-                Nub.transform.localPosition = Vector3.zero;
-                Joystick.SetActive(false);
-                B_Joystick_Visible = false;
-            }
             if (touch.phase == TouchPhase.Ended)
             {
-                if (Nub.transform.localPosition.magnitude > 45)
-                {
-                    if (Nub.transform.localPosition.x > 0)
-                    {
-                        if (Nub.transform.localPosition.y > 0) { Debug.Log("topRight"); }
-                        else { Debug.Log("bottomRight"); }
-                    }
-                    else
-                    {
-                        if (Nub.transform.localPosition.y > 0) { Debug.Log("topLeft"); }
-                        else { Debug.Log("bottomLeft"); }
-                    }
-
-                }
-                Nub.transform.localScale = Vector3.one;
-                Nub.transform.localPosition = Vector3.zero;
-                Joystick.SetActive(false);
-                B_Joystick_Visible = false;
+                ButtonSelected(45);
             }
         }
     }
 
 
-       
 
 
-void ShrinkNub()
+
+    void ShrinkNub()
     {
         if (!B_Joystick_Visible) { return; }
         Nub.transform.localScale = Nub.transform.localScale - Shrinker;
-       
-        if (Nub.transform.localScale.magnitude > .85 ) { Invoke("ShrinkNub", .01f); }
+
+        if (Nub.transform.localScale.magnitude > .85) { Invoke("ShrinkNub", .01f); }
     }
 
 
- void RegionActionHandler()
+    void RegionActionHandler()
     {
         switch (OptionChosen)
         {
@@ -118,11 +78,37 @@ void ShrinkNub()
             case "BL":
                 Debug.Log("BL");
                 break;
-                
+
 
         }
     }
 
- 
+    void ButtonSelected(int NubDisplacement)
+    {
+        if (Nub.transform.localPosition.magnitude > NubDisplacement)
+        {
 
+
+            if (Nub.transform.localPosition.x > 0)
+            {
+                if (Nub.transform.localPosition.y > 0) { OptionChosen = "topRight"; }
+                else { OptionChosen = "bottomRight"; }
+            }
+            else
+            {
+                if (Nub.transform.localPosition.y > 0) { OptionChosen = "topLeft"; }
+                else { OptionChosen = "bottomLeft"; }
+            }
+
+        }
+        CloseJS();
     }
+    void CloseJS()
+    {
+        Nub.transform.localScale = Vector3.one;
+        Nub.transform.localPosition = Vector3.zero;
+        Joystick.SetActive(false);
+        B_Joystick_Visible = false;
+    }
+
+}
